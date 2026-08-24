e6eae62ea7e05d8e3a1d21330505cff85df4b8eb
YamixDEV <bkru2281@gmail.com>
2026-08-24 12:45:06 +0300

    fix(runtime): restore deterministic shutdown and fd ownership

--- experimental/libbox/service_windows.go
+++ experimental/libbox/service_windows.go
@@ -1,7 +1,14 @@
 package libbox
 
-import "os"
+import (
+	"os"
+	"syscall"
+)
 
 func dup(fd int) (nfd int, err error) {
 	return 0, os.ErrInvalid
+}
+
+func closeFd(fd int) error {
+	return syscall.Close(syscall.Handle(fd))
 }
